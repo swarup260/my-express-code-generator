be9a677fad3cdb4aa1ec25c14769114c3dc9cd79
swarup das <swarup.das@airpay.co.in>
2021-10-25 16:49:12 +0530

    condition check added in args is null

--- runner.js
+++ runner.js
@@ -1,7 +1,30 @@
 const { runner } = require('hygen')
 const Logger = require('hygen/lib/logger')
 const path = require('path')
+const chalk = require('chalk')
 const defaultTemplates = path.join(__dirname, '_templates')
+
+if (process.argv.slice(2).length == 0) {
+  console.log(chalk.green(`
+
+  /* ---------------------------------------------------------- */
+  /*     GENERATE NEW PROJECT SCAFFOLD                          */
+  /* ---------------------------------------------------------- */
+  
+  npm start start projectGenerator new
+  `));
+
+  console.log(chalk.green(`
+
+  /* ---------------------------------------------------------- */
+  /*     GENERATE NEW CONTROLLER SCAFFOLD                       */
+  /* ---------------------------------------------------------- */
+
+  npm start start projectGenerator controller
+  `));
+  // Exit
+  return
+}
 
 runner(process.argv.slice(2), {
   templates: defaultTemplates,

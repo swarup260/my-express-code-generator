0e544c169e3be346cf74fcae61446a957ffa1a7a
Swarup Das <swarup.das@airpay.com>
2021-11-22 21:58:17 +0530

    new lib added

--- src/index.js
+++ src/index.js
@@ -1,20 +1,59 @@
 import { Command } from 'commander/esm.mjs';
-const program = new Command();
-
-
+import inquirer from 'inquirer';
+import Listr from 'listr';
+import ora from 'ora';
 
 function parseArgumentsIntoOptions(rawArgs) {
+    const program = new Command();
     program
-        .option('-d, --debug', 'output extra debugging')
-        .option('-s, --small', 'small pizza size')
-        .option('-p, --pizza-type <type>', 'flavour of pizza');
+        .option('-p, --project-name', 'project name')
+        .option('-v,--version', '0.1.0');
 
     program.parse(rawArgs);
 
     return program.opts()
 }
 
-export function cli(options) {
+async function promptForMissingOptions(rawArgs) {
+    let questions = [
+        {
+            type: 'input',
+            name: 'first_name',
+            message: "What's your first name",
+        }
+    ]
+
+    return await inquirer.prompt(questions);
+
+}
+
+async function taskRunner(options) {
+
+    const spinner = ora('').start();
+
+    const tasks = new Listr([
+        {
+            title: 'Success',
+            task: () => {
+                setTimeout(() => {
+                    spinner.color = 'yellow';
+                    spinner.stop()
+                }, 1000);
+
+            }
+        },
+        {
+            title: 'Failure',
+            task: () => 'Failure'
+        }
+    ]);
+
+    return await tasks.run()
+}
+export async function cli(options) {
     let parseOptions = parseArgumentsIntoOptions(options)
-    console.log(parseOptions)
+    let promptgOptions = await promptForMissingOptions(options)
+    console.log({ ...parseOptions, ...promptgOptions })
+    taskRunner({ ...parseOptions, ...promptgOptions })
+
 }

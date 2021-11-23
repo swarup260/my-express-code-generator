6371e17cbd4ee65ebf16ef2b5bb556d0e1c95c54
swarup <swarup260@gmail.com>
2021-11-23 23:33:47 +0530

    tasks added

--- src/index.js
+++ src/index.js
@@ -3,58 +3,142 @@ import inquirer from 'inquirer';
 import Listr from 'listr';
 import boxen from 'boxen';
 import chalk from 'chalk'
-import { buildTemplate } from './processes'
-import path from 'path'
+import { buildTemplate, gitInit, installPackage, generateJWTKeys,setupDocker } from "./processes";
 
+/**
+ * 
+ * @param {Object} rawArgs 
+ * @returns Object
+ */
 function parseArgumentsIntoOptions(rawArgs) {
     const program = new Command();
     program
-        .option('-p, --project-name', 'project name')
-        .option('-v,--version', '0.1.0');
+        .description('scaffold project/controller for existing project')
+        .option('-p, --project-name <value>', 'project name')
+        .option('-c, --controller-name <value>', 'controller name')
+        .option('-g,--git <boolean>', 'git init', false)
+        .option('-i,--install', 'npm package installation', false)
+        .version('0.0.1', '-v, --version', 'output the current version');
 
     program.parse(rawArgs);
 
     return program.opts()
 }
 
+/**
+ * 
+ * @param {Object} rawArgs 
+ * @returns Object
+ */
 async function promptForMissingOptions(rawArgs) {
     let questions = [
         {
-            type: 'input',
-            name: 'first_name',
-            message: "What's your first name",
+            type: 'list',
+            name: 'authentication',
+            message: "Authentication  type ?",
+            choices: ["JWT", "JWT-RSA", "JWT-ECDSA"],
+            default: "JWT"
+        },
+        {
+            type: 'confirm',
+            name: 'docker',
+            message: "setup docker ?"
         }
     ]
+    if (rawArgs.projectName) {
+        questions.unshift({
+            type: 'input',
+            name: 'projectDescription',
+            message: "project description ?"
+        })
+    }
 
-    return await inquirer.prompt(questions);
+    const options = await inquirer.prompt(questions)
+
+    return {
+        ...rawArgs,
+        ...options,
+        path: process.cwd()
+    }
 
 }
 
+
+/**
+ * 
+ * @param {Object} rawArgs 
+ * @returns Array
+ */
+function parseProjectTemplateOptions(rawArgs) {
+    if (!rawArgs.projectName) {
+        throw new Error('project name undefined')
+    }
+    return [
+        'generate', 'project',
+        '--projectName', rawArgs.projectName,
+        '--projectDescription', rawArgs.projectDescription,
+        '--path', rawArgs.path,
+        '--authentication', rawArgs.authentication
+    ]
+}
+
+/**
+ * 
+ * @param {Object} rawArgs 
+ * @returns Array
+ */
+function parseControllerTemplateOptions(rawArgs) {
+    if (!rawArgs.projectName) {
+        throw new Error('controller name undefined')
+    }
+    return [
+        'generate', 'controller',
+        '--name', rawArgs.controllerName,
+        '--path', rawArgs.path
+    ]
+}
+
+/**
+ * 
+ * @param {Object} options 
+ * 
+ */
 async function taskRunner(options) {
-    const fullPathName = new URL(import.meta.url).pathname;
-    const templateDir = path.resolve(
-      fullPathName.substr(fullPathName.indexOf('/')),
-      '../../templates',
-      ''
-    );
-    console.log(templateDir);
     const tasks = new Listr([
-        // {
-        //     title: 'Templates Generating',
-        //     task: () => buildTemplate(['generate', 'controller', '--name', 'user', '--path'])
-        // },
+        {
+            title: 'Project Templates Generating',
+            task: async () => await buildTemplate(parseProjectTemplateOptions(options)),
+            skip: () => options.projectName === undefined,
+        },
+        {
+            title: 'Controller Templates Generating',
+            task: async () => await buildTemplate(parseControllerTemplateOptions(options)),
+            skip: () => options.controllerName === undefined,
+        },
         {
             title: 'JWT Token Generating',
-            skip: () => options.jwtType != 'secret',
-            task: () => 'Success'
+            skip: () => options.authentication === 'JWT',
+            task: async () => await generateJWTKeys(options)
         },
         {
             title: 'Git Initailization',
-            task: () => 'Failure'
+            task: async () => await gitInit(),
+            enabled: () => options.git
+        },
+        {
+            title: 'Docker Setup',
+            skip: () => options.docker === false,
+            task: async () => await setupDocker(options)
         },
         {
             title: 'Package Installation',
-            task: () => 'Failure'
+            task: async () => await installPackage({
+                cwd: process.cwd()
+            }),
+            skip: () =>
+                !options.install
+                    ? 'Pass --install to automatically install dependencies'
+                    : undefined,
         },
     ]);
 
@@ -62,9 +146,13 @@ async function taskRunner(options) {
     console.log(boxen(chalk.green('npm run dev'), { padding: 1, margin: 1, borderStyle: 'double' }));
 }
 
+/**
+ * 
+ * @param {Object*} options 
+ */
 export async function cli(options) {
     let parseOptions = parseArgumentsIntoOptions(options)
-    let promptgOptions = await promptForMissingOptions(options)
-    console.log({ ...parseOptions, ...promptgOptions })
+    let promptgOptions = await promptForMissingOptions(parseOptions)
+    console.log(promptgOptions);
     await taskRunner({ ...parseOptions, ...promptgOptions })
 }
--- src/processes.js
+++ src/processes.js
@@ -1,28 +1,104 @@
+import { runner, Logger } from "hygen";
+import path from "path";
+import { projectInstall } from "pkg-install";
+import crypto from "crypto";
+import { promisify } from "util";
+import fs from "fs";
+import { config } from "process";
 
-const { runner } = require('hygen')
-const Logger = require('hygen/lib/logger')
-const path = require('path')
-const defaultTemplates = path.join(__dirname, '_templates')
+const generateKeyPair = promisify(crypto.generateKeyPair)
+const writeFile = promisify(fs.writeFile)
 
-// ['generate', 'controller', '--name', 'user', '--path', '.']
-
-
-const buildTemplate = rawArgs => runner(rawArgs, {
-    // runner(process.argv.slice(2), {
-    templates: defaultTemplates,
-    cwd: process.cwd(),
-    logger: new Logger(console.log.bind(console)),
-    createPrompter: () => require('enquirer'),
-    exec: (action, body) => {
-        const opts = body && body.length > 0 ? { input: body } : {}
-        return require('execa').shell(action, opts)
-    },
-    debug: !!process.env.DEBUG
-})
+const defaultTemplates = path.join(__dirname, '..\\_templates')
 
 
+/**
+ * 
+ * @param {Object} rawArgs 
+ */
+async function buildTemplate(rawArgs) {
+    await runner(rawArgs, {
+        templates: defaultTemplates,
+        cwd: process.cwd(),
+        logger: new Logger(console.log.bind(console)),
+        createPrompter: () => { },
+        exec: (action, body) => {
+            const opts = body && body.length > 0 ? { input: body } : {}
+            return require('execa').shell(action, opts)
+        },
+        debug: !!process.env.DEBUG
+    })
+
+}
+
+/**
+ * 
+ * @param {Object} rawArgs 
+ */
+async function gitInit(rawArgs) {
+}
+
+/**
+ * 
+ * @param {Object} rawArgs 
+ */
+async function generateJWTKeys(rawArgs) {
+    try {
+
+        const CONFIG = {
+            modulusLength: 4096,
+            publicKeyEncoding: {
+                type: 'spki',
+                format: 'pem'
+            },
+            privateKeyEncoding: {
+                type: 'pkcs8',
+                format: 'pem'
+            }
+        }
+        if (rawArgs.authentication === "JWT-ECDSA") {
+            CONFIG['namedCurve'] = 'sect239k1'
+        }
+
+        const AUTH_TYPE = rawArgs.authentication === "JWT-ECDSA" ? "ec" : "rsa"
+
+        const {
+            publicKey,
+            privateKey,
+        } = await generateKeyPair(AUTH_TYPE, CONFIG);
+
+        await writeFile(`${rawArgs.path}/privateKey.pem`, privateKey)
+        await writeFile(`${rawArgs.path}/publicKey.pem`, publicKey)
+    } catch (error) {
+        throw error
+    }
 
 
-module.exports = {
-    buildTemplate
+
+}
+
+/**
+ * 
+ * @param {Object} rawArgs 
+ */
+async function setupDocker(rawArgs) {
+
+}
+
+/**
+ * 
+ * @param {Object} rawArgs 
+ */
+async function installPackage(rawArgs) {
+    await projectInstall(rawArgs)
+}
+
+
+
+export {
+    buildTemplate,
+    gitInit,
+    generateJWTKeys,
+    installPackage,
+    setupDocker
 }
